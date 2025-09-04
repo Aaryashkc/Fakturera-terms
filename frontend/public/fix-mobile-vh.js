067f635acc98ce412b63b7d130ce5c5bf23517cb
Aaryashkc <aaryashkc123@gmail.com>
2025-09-04 19:50:54 +0545

    mobile

--- frontend/public/fix-mobile-vh.js
+++
@@ -1,8 +0,0 @@
-// This script fixes mobile viewport height issues for backgrounds
-function setVh() {
-  const vh = window.innerHeight * 0.01;
-  document.documentElement.style.setProperty('--vh', `${vh}px`);
-}
-setVh();
-window.addEventListener('resize', setVh);
-window.addEventListener('orientationchange', setVh);
