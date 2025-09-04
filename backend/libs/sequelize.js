81320689c255c1510b1d573672dbc9f24e01f6ae
Aaryashkc <aaryashkc123@gmail.com>
2025-09-04 14:52:36 +0545

    changed ipv4 from ipv6

--- backend/libs/sequelize.js
+++ backend/libs/sequelize.js
@@ -1,7 +1,10 @@
 import { Sequelize } from "sequelize";
 import dotenv from "dotenv";
+import dns from 'node:dns';
 
 dotenv.config();
+
+dns.setDefaultResultOrder('ipv4first');
 
 const sequelize = new Sequelize(process.env.SUPABASE_DB_URL, {
   dialect: "postgres",
