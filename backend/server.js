6f12aa183815fd5e32a5a686114d6280a9434cf4
Aaryashkc <aaryashkc123@gmail.com>
2025-09-04 14:49:59 +0545

    minor adjustments

--- backend/server.js
+++ backend/server.js
@@ -10,7 +10,7 @@ const port = process.env.PORT
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
 app.use(cors({
-    origin: ["http://localhost:5173"],
+    origin: ["http://localhost:5173", "https://fakturera-terms-eight.vercel.app"],
     credentials: true, 
 }
 ))
--- frontend/src/Store/useTermsStore.js
+++ frontend/src/Store/useTermsStore.js
@@ -1,6 +1,10 @@
 import { create } from "zustand";
 import axios from "axios";
 
+const API_BASE_URL = import.meta.env.MODE === "development" 
+? 'http://localhost:5001' 
+: "https://fakturera-terms-2.onrender.com"
+
 export const useTermsStore = create((set) => ({
   terms: { lang: "en", title: "", button: "", text: "" },
   loading: false,
@@ -9,7 +13,7 @@ export const useTermsStore = create((set) => ({
   fetchTerms: async (lang = "en") => {
     set({ loading: true, error: null });
     try {
-      const res = await axios.get(`http://localhost:5001/api/terms?lang=${lang}`);
+      const res = await axios.get(`${API_BASE_URL}/api/terms?lang=${lang}`);
       set({ terms: res.data, loading: false });
     } catch (err) {
       console.error(err);
