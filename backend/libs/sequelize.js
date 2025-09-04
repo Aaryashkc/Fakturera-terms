1d00f0a49da9237cf8ad60d093ec46a8b308ffce
Aaryashkc <aaryashkc123@gmail.com>
2025-09-04 15:20:04 +0545

    changes

--- backend/libs/sequelize.js
+++ backend/libs/sequelize.js
@@ -1,19 +1,7 @@
 import { Sequelize } from "sequelize";
 import dotenv from "dotenv";
-import dns from 'node:dns';
-import pg from 'pg';
 
 dotenv.config();
-
-// Prefer IPv4 addresses to avoid IPv6 ENETUNREACH in some environments (e.g., Render)
-try {
-  dns.setDefaultResultOrder('ipv4first');
-} catch {}
-
-// Force pg to resolve DB host to IPv4
-pg.defaults.lookup = (host, options, callback) => {
-  return dns.lookup(host, { family: 4 }, callback);
-};
 
 
 const sequelize = new Sequelize(process.env.SUPABASE_DB_URL, {
